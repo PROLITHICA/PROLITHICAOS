035870f19b41b2e2c4e8335996e9d6c1a65ea974
newtvn <newtonbryan12428@gmail.com>
2026-09-11 12:06:04 +0300

    Use clean path routing

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,7 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter, withComponentInputBinding, withInMemoryScrolling, withHashLocation } from '@angular/router';
-import { deployment } from './core/deployment';
+import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { tokenInterceptor } from './core/token.interceptor';
@@ -12,7 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
-      ...(deployment.pages ? [withHashLocation()] : []),
       withComponentInputBinding(),
       withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
     ),
